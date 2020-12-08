67bc1dc7c94c6c3c668754c020291f946893c58c
jdrojo2018 <drojomartin@gmail.com>
2020-12-08 11:46:40 +0100

    Temporizador para iniciar la Muerte Súbita y detalles menores

--- src/js/Scene2.js
+++ src/js/Scene2.js
@@ -5,18 +5,24 @@ class Scene2 extends Phaser.Scene {
 
     //No hace falta que estén aquí las variables, pero me gusta saber que tengo aquí.
     playersGroup;
-    player;
+    playersList;
 
     ballsList;
     ballsGroup;
+
+    maxBallsInScene = 10;
+
+
+    minutesRemaining = 2;
+    secondsRemaining = 30;
 
     create() {
         this.background = this.add.tileSprite(0,0, config.width, config.height,"background");
         this.background.setOrigin(0,0);
         
-        this.add.text(20, 20, "Playing game", {
-            font: "50px Arial", 
-            fill: "yellow",
+        this.matchTimer = this.add.text(20, 20, "Playing game", {
+            font: "25px Arial", 
+            fill: "white",
             
         });
         
@@ -30,28 +36,45 @@ class Scene2 extends Phaser.Scene {
 
     setupPlayers(){
         this.playersGroup = this.add.group();
-        this.player = new Player(this, config.width / 2, config.height / 2);
+
+        this.playersList = [];
+
+        this.playersList[0] = new Player(this, config.width / 2, config.height / 2);
     }
 
     setupInitialBalls(){
         this.ballsList = [];
-        for (var i = 0; i < 5; i++) {
-            if(Phaser.Math.Between(0, 10) > 5){
-                this.ballsList[i] = new Ball(this, Phaser.Math.Between(0, config.width), Phaser.Math.Between(0, config.height), "bomba");
-            }else{
-                this.ballsList[i] = new BallTal(this, Phaser.Math.Between(0, config.width), Phaser.Math.Between(0, config.height));
-            }
-            
+
+        for (var i = 0; i < this.maxBallsInScene; i++) {
+            this.generateNewBall();
         }
     
         this.ballsGroup = this.add.group();
         this.physics.add.collider(this.playersGroup, this.ballsGroup, this.colisionPlayerBall);
+
+        var timedEvent = this.time.addEvent({ delay: 1000, callback:  this.ballRespawnerTimerEnded, callbackScope: this, loop: true });
+    }
+
+    ballRespawnerTimerEnded(){
+        if(this.ballsList.length < this.maxBallsInScene){
+            this.generateNewBall();
+        }
+    }
+
+    generateNewBall(){
+        var i = this.ballsList.length;
+
+        if(Phaser.Math.Between(0, 10) > 4){
+            this.ballsList[i] = new Ball(this, Phaser.Math.Between(0, config.width), Phaser.Math.Between(0, config.height), "bomba");
+        }else{
+            this.ballsList[i] = new BallTal(this, Phaser.Math.Between(0, config.width), Phaser.Math.Between(0, config.height));
+        }
     }
 
     //Cuando un boloncio choqua contra un jugador
     colisionPlayerBall(player, ball){
         ball.impact();
-        //Hostiar al afortunado
+
         player.takeDamage();
     }
 
@@ -59,8 +82,54 @@ class Scene2 extends Phaser.Scene {
 
         this.background.tilePositionY -= 0.5;
 
-        this.player.update(delta);
+        this.updatePlayers(delta);
+        this.updateBalls(delta);
+
+        this.updateClock(time, delta);
+    }
+
+    acumulatedDelta = 0;
+
+    timeEnded = false;
+
+    updateClock(time, delta){
+        if(this.timeEnded)
+            return;
         
+        this.acumulatedDelta += delta;
+
+        if(this.acumulatedDelta >= 200){
+            this.secondsRemaining -= 1;
+
+
+            if(this.secondsRemaining == -1 && this.minutesRemaining > 0){
+                this.secondsRemaining = 59;
+                this.minutesRemaining -= 1;
+            }
+
+            if(this.secondsRemaining == 0 && this.minutesRemaining == 0){
+                this.timeEnded = true;
+                console.log("EL FINAL DE TODOOOOOOOOOOOOO");
+            }
+            if(this.secondsRemaining < 10){
+                this.matchTimer.setText(this.minutesRemaining+":0"+this.secondsRemaining);
+            }else{
+                this.matchTimer.setText(this.minutesRemaining+":"+this.secondsRemaining);
+            }
+            
+
+            this.acumulatedDelta = 0;
+        }
+       
+    }
+
+    updatePlayers(delta){
+        for (var i = this.playersList.length - 1; i >= 0; i--) {
+            this.playersList[i].update(delta);
+        }
+    }
+
+    updateBalls(delta){
         for (var i = this.ballsList.length - 1; i >= 0; i--) {
             this.ballsList[i].update(delta);
         }
